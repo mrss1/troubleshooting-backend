e739232e4388bd4678d4518e372eea6887119cc6
Ross <Michael.Ross@mail.house.gov>
2024-11-20 10:55:30 -0500

    update knexfile

--- knexfile.js
+++ knexfile.js
@@ -4,13 +4,16 @@ require('dotenv').config();
 module.exports = {
   client: 'pg',
   connection: {
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+      rejectUnauthorized: false, // Required for Render's database
+    },
   },
   migrations: {
     directory: './migrations',
   },
+  seeds: {
+    directory: './seeds',
+  },
 };
+
